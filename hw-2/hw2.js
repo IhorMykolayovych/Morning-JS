8fd7dfdd66423289955e3a77653f75c02438d610
Ihor Mykolayovych <118132467+igniko@users.noreply.github.com>
2024-04-30 09:18:53 +0300

    Add files via upload

---
+++ hw-2/hw2.js
@@ -0,0 +1,47 @@
+// Oголошення змінної var
+var batterConsistency;
+var patelniaTemperature;
+var cookingTime;
+var flipCount;
+var ingredientList;
+var batterVolume;
+var servingSize;
+var toppingChoice;
+var remainingBatter;
+var patelniaOilLevel;
+
+// Oголошення змінної let
+let batterConsistency;
+let patelniaTemperature;
+let cookingTime;
+let flipCount;
+let ingredientList;
+let batterVolume;
+let servingSize;
+let toppingChoice;
+let remainingBatter;
+let patelniaOilLevel;
+
+// Ініціалізація змінних
+batterConsistency = "homogeneous";
+patelniaTemperature = "120—180";
+cookingTime = "50";
+flipCount = "none";
+ingredientList = "arbitrary";
+batterVolume = "1/3";
+servingSize = "90-100";
+toppingChoice = "false";
+remainingBatter = "true";
+patelniaOilLevel = "10-20%";
+
+// Oголошення та ініціалізація змінної константи (const)
+const batterConsistency = "homogeneous";
+const patelniaTemperature = "120—180";
+const cookingTime = "50";
+const flipCount = "none";
+const ingredientList = "arbitrary";
+const batterVolume = "1/3";
+const servingSize = "90-100";
+const toppingChoice = "false";
+const remainingBatter = "true";
+const patelniaOilLevel = "10-20%";
---
+++ hw-4/hw4.js
@@ -0,0 +1,24 @@
+// Ініціалізація числових змінних:
+let integerNumber = 1426;
+console.log(integerNumber);
+
+let floatingPointNumber = 19.14;
+console.log(floatingPointNumber);
+
+// Робота з великими числами:
+const largeNumber = 1_000_000;
+console.log(largeNumber);
+
+// Експоненціальний запис чисел:
+let scientificNotation = 1.23e5; // цей запис означає  1.23 * 10^5
+console.log(scientificNotation);
+
+// Використання об'єкту Math для генерації випадкових чисел:
+let randomNumber = Math.random();
+console.log(randomNumber); // 0.7527361837692697
+
+// Особливості чисел у JavaScript:
+let firstDecimal = 0.11;
+let secondDecimal = 0.12;
+let sum = firstDecimal + secondDecimal;
+console.log(sum); // 0.22999999999999998
